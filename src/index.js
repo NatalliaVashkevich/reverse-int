8b13f6e08b8f77d76ec818b6400aa5ae00b491aa
Natallia_Vashkevich <mahatali84@gmail.com>
2024-08-14 15:26:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let numToString = n.toString();
+  if (n >= 0) {
+   for (let i = 0; i < numToString.length; i++) {
+    result = `${numToString[i]}${result}`;
+  }
+    return +result;
+  } else if (n < 0) {
+    for (let i = 1; i < numToString.length; i++) {
+    result = `${numToString[i]}${result}`;
+  }
+    return (+result * -1);
+ }
+   
 }
+
+
